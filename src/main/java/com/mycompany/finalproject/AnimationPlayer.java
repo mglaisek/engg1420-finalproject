326f9822306569a782dd93affef37d6b7039c5fb
mylog <mylog@10.12.89.201>
2022-03-28 13:14:04 -0400

    began writing animationplayer

---
+++ src/main/java/com/mycompany/finalproject/AnimationPlayer.java
@@ -0,0 +1,47 @@
+package com.mycompany.finalproject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class AnimationPlayer extends Application {
+    int numFrames, fps, numAnimations;
+
+    public AnimationPlayer()    {
+    }
+
+    public loadAnimationFromFile(String filename)   {
+        File file = new File(filename);
+        try {
+            Scanner sc = new Scanner(file);
+            numFrames = sc.nextInt();
+            fps = sc.nextInt();
+            numAnimations = sc.nextInt();
+        }
+    }
+    
+    public run()    {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        var javaVersion = SystemInfo.javaVersion();
+        var javafxVersion = SystemInfo.javafxVersion();
+        
+        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
